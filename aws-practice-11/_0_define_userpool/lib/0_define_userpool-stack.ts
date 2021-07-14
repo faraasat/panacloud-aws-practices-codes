dd5ab167ab413522dee4de4ed05ea790b880a8db
Farasat Ali <farasataliazeemi1@gmail.com>
2021-07-14 12:41:58 +0500

    Updated the App

---
+++ aws-practice-11/_0_define_userpool/lib/0_define_userpool-stack.ts
@@ -0,0 +1,42 @@
+import * as cdk from "@aws-cdk/core";
+import * as cognito from "@aws-cdk/aws-cognito";
+
+export class _0DefineUserpoolStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const userPool = new cognito.UserPool(this, "UserPool", {
+      selfSignUpEnabled: true, // Allow users to sign up
+      autoVerify: { email: true }, // Verify email addresses by sending a verification code
+      signInAliases: { email: true }, // Set email as an alias
+      userVerification: {
+        emailSubject: "Verify your email for our awesome app!",
+        emailBody:
+          "Hello {username}, Thanks for signing up to our awesome app! Your verification code is {####}",
+        emailStyle: cognito.VerificationEmailStyle.CODE,
+      }, ///customize email and sms
+      standardAttributes: {
+        fullname: {
+          required: true,
+          mutable: false,
+        },
+      }, ////Attributes already define by cognito
+      customAttributes: {
+        myappid: new cognito.StringAttribute({
+          minLen: 5,
+          maxLen: 15,
+          mutable: false,
+        }),
+      }, ////Custom Attributes defined according to the application needs
+      passwordPolicy: {
+        minLength: 12,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireDigits: true,
+        requireSymbols: true,
+        tempPasswordValidity: cdk.Duration.days(3),
+      }, ///Password Policy
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY, ///Account Recovery email
+    });
+  }
+}
