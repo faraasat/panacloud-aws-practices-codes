074bade0394255d9625cbe265d0bd5469a23e7b6
Farasat Ali <farasataliazeemi1@gmail.com>
2021-07-19 20:12:44 +0500

    Updated backend

--- aws-practice-11/_03_google_login_cognito_amplify/backend/lib/backend-stack.ts
+++ aws-practice-11/_03_google_login_cognito_amplify/backend/lib/backend-stack.ts
@@ -1,9 +1,71 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as cognito from "@aws-cdk/aws-cognito";
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const userPool = new cognito.UserPool(this, "eruGoogleUserPool", {
+      selfSignUpEnabled: true,
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+      userVerification: {
+        emailStyle: cognito.VerificationEmailStyle.CODE,
+      },
+      autoVerify: {
+        email: true,
+      },
+      standardAttributes: {
+        email: {
+          required: true,
+          mutable: true,
+        },
+      },
+    });
+
+    const provider = new cognito.UserPoolIdentityProviderGoogle(
+      this,
+      "googleProvider",
+      {
+        userPool: userPool,
+        clientId:
+          "12393919978-rjnn89e3up4gpjnm2uboj3o4sgb64rci.apps.googleusercontent.com", // Google Client id
+        clientSecret: "8QTXkEw_1TuclNUDpnfVh0Yg", // Google Client Secret
+        attributeMapping: {
+          email: cognito.ProviderAttribute.GOOGLE_EMAIL,
+          givenName: cognito.ProviderAttribute.GOOGLE_GIVEN_NAME,
+          phoneNumber: cognito.ProviderAttribute.GOOGLE_PHONE_NUMBERS,
+        },
+        scopes: ["profile", "email", "openid"],
+      }
+    );
+    userPool.registerIdentityProvider(provider);
+
+    const userPoolClient = new cognito.UserPoolClient(this, "amplifyClient", {
+      userPool,
+      oAuth: {
+        callbackUrls: ["http://localhost:8000/"], // This is what user is allowed to be redirected to with the code upon signin. this can be a list of urls.
+        logoutUrls: ["http://localhost:8000/"], // This is what user is allowed to be redirected to after signout. this can be a list of urls.
+      },
+    });
+
+    const domain = userPool.addDomain("domain", {
+      cognitoDomain: {
+        domainPrefix: "bootcamp-mognito-mine", // SET YOUR OWN Domain PREFIX HERE
+      },
+    });
+
+    new cdk.CfnOutput(this, "aws_user_pools_web_client_id", {
+      value: userPoolClient.userPoolClientId,
+    });
+    new cdk.CfnOutput(this, "aws_project_region", {
+      value: this.region,
+    });
+    new cdk.CfnOutput(this, "aws_user_pools_id", {
+      value: userPool.userPoolId,
+    });
+
+    new cdk.CfnOutput(this, "domain", {
+      value: domain.domainName,
+    });
   }
 }
