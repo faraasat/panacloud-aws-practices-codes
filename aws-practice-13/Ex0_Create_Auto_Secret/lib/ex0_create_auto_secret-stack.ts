c69b05e7407cfc4c806775e9ebcf0edd13d1e147
Farasat Ali <farasataliazeemi1@gmail.com>
2021-08-01 00:00:03 +0500

    Updated Ex0_Create_Auto_Secret

--- aws-practice-13/Ex0_Create_Auto_Secret/lib/ex0_create_auto_secret-stack.ts
+++ aws-practice-13/Ex0_Create_Auto_Secret/lib/ex0_create_auto_secret-stack.ts
@@ -1,32 +1,51 @@
 import * as cdk from "@aws-cdk/core";
 import * as secretsmanager from "@aws-cdk/aws-secretsmanager";
-import * as iam from "@aws-cdk/aws-iam";
 import * as lambda from "@aws-cdk/aws-lambda";
 
 export class Ex0CreateAutoSecretStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const role = new iam.Role(this, "LambdaRole", {
-      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+    // to generate secret value
+    const secret = new secretsmanager.Secret(this, "Secret", {
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({
+          username: "aamir",
+          password: "12345",
+        }),
+        generateStringKey: "randomKey", // to generate random key
+      },
     });
 
-    const secret = new secretsmanager.Secret(this, "Secret"); // SecretsManager generate a new secret value automatically
+    const secretValue = secretsmanager.Secret.fromSecretNameV2(
+      this,
+      "SecretValueID",
+      secret.secretName
+    ).secretValue;
 
+    // Same as above but using arn
+    // const secretValue = secretsmanager.Secret.fromSecretAttributes(
+    //   this,
+    //   "ExampleSecretKey",
+    //   {
+    //     secretArn: secret.secretArn,
+    //   }
+    // ).secretValue;
+
+    // to use this secret in lambda
     const lambdaFn = new lambda.Function(this, `ExampleLambdaAssetFn`, {
+      // We can write this inline lambda code properly using fromAsset
       code: lambda.Code.fromInline(
         'exports.handler = function(event, ctx, cb) { console.log("SECRET_KEY", process.env.EXAMPLE_SECRET_KEY); return cb(null, "hi"); }'
       ),
       runtime: lambda.Runtime.NODEJS_12_X,
-      role: role,
-      environment: {
-        EXAMPLE_SECRET_KEY: `${
-          secretsmanager.Secret.fromSecretAttributes(this, "ExampleSecretKey", {
-            secretArn: secret.secretArn,
-          }).secretValue
-        }`,
-      },
+      // environment: {
+      //   EXAMPLE_SECRET_KEY: secretValue.toString(),
+      // },
       handler: "index.handler",
     });
+
+    // Same as we used in environment
+    lambdaFn.addEnvironment("EXAMPLE_SECRET_KEY", secretValue.toString());
   }
 }
