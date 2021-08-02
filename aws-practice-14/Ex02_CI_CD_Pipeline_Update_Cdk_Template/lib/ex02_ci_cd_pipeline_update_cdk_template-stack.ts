9f0ca681628372997ac9f744d8f16a08ca1872dc
Farasat Ali <farasataliazeemi1@gmail.com>
2021-08-02 18:06:10 +0500

    Updated Ex02_CI_CD_Pipeline_Update_Cdk_Template

--- aws-practice-14/Ex02_CI_CD_Pipeline_Update_Cdk_Template/lib/ex02_ci_cd_pipeline_update_cdk_template-stack.ts
+++ aws-practice-14/Ex02_CI_CD_Pipeline_Update_Cdk_Template/lib/ex02_ci_cd_pipeline_update_cdk_template-stack.ts
@@ -7,7 +7,7 @@ export class Ex02CiCdPipelineUpdateCdkTemplateStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Artifact from source stage
+    // Artifact from source stage. Artifact is where we put our things
     const sourceOutput = new CodePipeline.Artifact();
 
     // Artifact from build stage
@@ -16,6 +16,7 @@ export class Ex02CiCdPipelineUpdateCdkTemplateStack extends cdk.Stack {
     //Code build action, Here you will define a complete build
     const cdkBuild = new CodeBuild.PipelineProject(this, "CdkBuild", {
       buildSpec: CodeBuild.BuildSpec.fromObject({
+        // how project will run and its packages will installed
         version: "0.2",
         phases: {
           install: {
@@ -23,67 +24,70 @@ export class Ex02CiCdPipelineUpdateCdkTemplateStack extends cdk.Stack {
               nodejs: 12,
             },
             commands: [
-              "cd step12_CI_CD_pipeline",
-              "cd Ex02_CI_CD_Pipeline_Update_Cdk_Template",
+              // "cd step12_CI_CD_pipeline",
+              // "cd Ex02_CI_CD_Pipeline_Update_Cdk_Template",
               "npm install",
             ],
           },
           build: {
-            commands: ["npm run build", "npm run cdk synth -- -o dist"],
+            commands: ["npm run build", "npm run cdk synth -- -o dist"], // cdk synth makes cdk.out folder so we use it to create output without deploying code
           },
         },
         artifacts: {
-          "base-directory":
-            "./step12_CI_CD_pipeline/step02_CI_CD_pipeline_update_cdk_template/dist", ///outputting our generated JSON CloudFormation files to the dist directory
-          files: [`${this.stackName}.template.json`],
+          "base-directory": "./dist", // outputting our generated JSON CloudFormation files to the dist directory
+          // "./step12_CI_CD_pipeline/step02_CI_CD_pipeline_update_cdk_template/dist",
+          files: [`${this.stackName}.template.json`], // Which file to use, this represent class and stack name is the variable of it
         },
       }),
       environment: {
-        buildImage: CodeBuild.LinuxBuildImage.STANDARD_3_0, ///BuildImage version 3 because we are using nodejs environment 12
+        // On which OS we want to install
+        buildImage: CodeBuild.LinuxBuildImage.STANDARD_3_0, // BuildImage version 3 because we are using nodejs environment 12
       },
     });
 
     ///Define a pipeline
     const pipline = new CodePipeline.Pipeline(this, "CDKPipeline", {
-      crossAccountKeys: false, //Pipeline construct creates an AWS Key Management Service (AWS KMS) which cost $1/month. this will save your $1.
-      restartExecutionOnUpdate: true, //Indicates whether to rerun the AWS CodePipeline pipeline after you update it.
+      crossAccountKeys: false, // Pipeline construct creates an AWS Key Management Service (AWS KMS) which cost $1/month. this will save your $1. Also we have one account so crossAccountKeys is not required.
+      restartExecutionOnUpdate: true, // Indicates whether to rerun the AWS CodePipeline pipeline after you update it.
     });
 
-    ///Adding stages to pipeline
-
-    //First Stage Source
+    // Adding stages to pipeline
+    // First Stage, Source of Code
     pipline.addStage({
       stageName: "Source",
       actions: [
         new CodePipelineAction.GitHubSourceAction({
           actionName: "Checkout",
-          owner: "panacloud-modern-global-apps",
-          repo: "full-stack-serverless-cdk",
-          oauthToken: cdk.SecretValue.secretsManager("GITHUB_TOKEN_AWS_SOURCE"), ///create token on github and save it on aws secret manager
-          output: sourceOutput, ///Output will save in the sourceOutput Artifact
-          branch: "master", ///Branch of your repo
+          owner: "panacloud-modern-global-apps", // name of repository account
+          repo: "full-stack-serverless-cdk", // repository name
+          oauthToken: cdk.SecretValue.secretsManager("GITHUB_TOKEN_AWS_SOURCE"), // Create token on github and save it on aws secret manager. Used if there is a private repo. We make token in our repo and we can give the token rights. Since, we can use it from Secret manager we can also use here it plain text
+          output: sourceOutput, // Output will save in the sourceOutput Artifact. Here in this artifact files from github will be saved
+          branch: "master", // Branch of your repo
         }),
       ],
     });
 
+    // Second Stage, Code Building
     pipline.addStage({
       stageName: "Build",
       actions: [
         new CodePipelineAction.CodeBuildAction({
           actionName: "cdkBuild",
-          project: cdkBuild,
+          project: cdkBuild, // Which steps of the project
           input: sourceOutput,
           outputs: [CDKOutput],
         }),
       ],
     });
 
+    // Third Stage, Deployment
     pipline.addStage({
       stageName: "DeployCDK",
       actions: [
         new CodePipelineAction.CloudFormationCreateUpdateStackAction({
+          // Here function shows where to deploy
           actionName: "AdministerPipeline",
-          templatePath: CDKOutput.atPath(`${this.stackName}.template.json`), ///Input artifact with the CloudFormation template to deploy
+          templatePath: CDKOutput.atPath(`${this.stackName}.template.json`), // Input artifact with the CloudFormation template to deploy
           stackName: this.stackName, ///Name of stack
           adminPermissions: true,
         }),
