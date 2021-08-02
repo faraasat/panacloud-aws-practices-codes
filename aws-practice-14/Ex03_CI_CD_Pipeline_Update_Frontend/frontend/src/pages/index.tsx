55d3ccbbbe291df723df40671323d26865e82b37
Farasat Ali <farasataliazeemi1@gmail.com>
2021-08-02 18:28:55 +0500

    Completed Ex03_CI_CD_Pipeline_Update_Frontend

---
+++ aws-practice-14/Ex03_CI_CD_Pipeline_Update_Frontend/frontend/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react"
+import Seo from "../components/seo"
+
+const IndexPage = () => {
+  return (
+    <>
+      <Seo title="Home" />
+      <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default IndexPage
