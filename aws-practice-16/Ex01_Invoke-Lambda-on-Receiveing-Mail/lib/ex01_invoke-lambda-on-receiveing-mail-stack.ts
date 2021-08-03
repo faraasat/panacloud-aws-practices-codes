2d9ca70c3c102774e6ee10f50595f9ab5a7fc088
Farasat Ali <farasataliazeemi1@gmail.com>
2021-08-03 16:22:49 +0500

    Update ex01_invoke-lambda-on-receiveing-mail-stack.ts

--- aws-practice-16/Ex01_Invoke-Lambda-on-Receiveing-Mail/lib/ex01_invoke-lambda-on-receiveing-mail-stack.ts
+++ aws-practice-16/Ex01_Invoke-Lambda-on-Receiveing-Mail/lib/ex01_invoke-lambda-on-receiveing-mail-stack.ts
@@ -23,14 +23,15 @@ export class Ex01InvokeLambdaOnReceiveingMailStack extends cdk.Stack {
 
     // creating instance for taking email input while deployment
     // ref https://docs.aws.amazon.com/cdk/latest/guide/parameters.html
-    const emailAddress = new cdk.CfnParameter(this, "emailParam", {
-      type: "String",
-      description: "Write your recipient email",
-    });
+    // const emailAddress = new cdk.CfnParameter(this, "emailParam", {
+    //   type: "String",
+    //   description: "Write your recipient email",
+    // });
 
     // Adding a rule inside a rule set
     ruleSet.addRule("INVOKE_LAMBDA_RULE", {
-      recipients: [emailAddress.valueAsString], // if no recipients than the action will be called on any incoming mail addresses of verified domains
+      // recipients: [emailAddress.valueAsString], // if no recipients than the action will be called on any incoming mail addresses of verified domains
+      recipients: ["aamirpinger@yahoo.com"],
       actions: [
         new actions.Lambda({
           // defining an action to call when receive email on given recipients
