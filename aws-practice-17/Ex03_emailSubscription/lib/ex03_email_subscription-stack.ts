ffe448812bae62eeef9c31032b9f8566abb89cf0
Farasat Ali <farasataliazeemi1@gmail.com>
2021-08-03 18:17:10 +0500

    Update ex03_email_subscription-stack.ts

--- aws-practice-17/Ex03_emailSubscription/lib/ex03_email_subscription-stack.ts
+++ aws-practice-17/Ex03_emailSubscription/lib/ex03_email_subscription-stack.ts
@@ -1,9 +1,27 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as subscriptions from "@aws-cdk/aws-sns-subscriptions";
+import * as sns from "@aws-cdk/aws-sns";
+import * as sqs from "@aws-cdk/aws-sqs";
 
 export class Ex03EmailSubscriptionStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // create an SNS topic
+    const myTopic = new sns.Topic(this, "MyTopic");
+
+    // create a dead letter queue
+    const dlQueue = new sqs.Queue(this, "DeadLetterQueue", {
+      queueName: "MySubscription_DLQ",
+      retentionPeriod: cdk.Duration.days(14),
+    });
+
+    // subscribe email to the topic
+    myTopic.addSubscription(
+      new subscriptions.EmailSubscription("ADD YOUR EMAIL HERE", {
+        json: false,
+        deadLetterQueue: dlQueue,
+      })
+    );
   }
 }
