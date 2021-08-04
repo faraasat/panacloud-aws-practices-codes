be9d079bc19a464cb625bdcf84052a2e1d045d81
Farasat Ali <farasataliazeemi1@gmail.com>
2021-08-05 00:07:04 +0500

    Update ex01_sqs_to_lambda-stack.ts

--- aws-practice-18/Ex01_sqs_to_lambda/lib/ex01_sqs_to_lambda-stack.ts
+++ aws-practice-18/Ex01_sqs_to_lambda/lib/ex01_sqs_to_lambda-stack.ts
@@ -1,9 +1,32 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as sqs from "@aws-cdk/aws-sqs";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as lambdaEvents from "@aws-cdk/aws-lambda-event-sources";
+import * as path from "path";
 
 export class Ex01SqsToLambdaStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const sqsLambda = new lambda.Function(this, "sqsLambda", {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset(path.join(__dirname, "/../", "lambda")),
+      handler: "index.handler",
+      reservedConcurrentExecutions: 5, // only have 5 invocations at a time, having this <5 has a problem?
+    });
+
+    const queue = new sqs.Queue(this, "testQueue", {
+      queueName: "testQueue",
+      encryption: sqs.QueueEncryption.UNENCRYPTED,
+      retentionPeriod: cdk.Duration.days(4),
+      visibilityTimeout: cdk.Duration.seconds(30), // default,
+      receiveMessageWaitTime: cdk.Duration.seconds(20), // default
+    });
+
+    sqsLambda.addEventSource(
+      new lambdaEvents.SqsEventSource(queue, {
+        batchSize: 10, // default
+      })
+    );
   }
 }
