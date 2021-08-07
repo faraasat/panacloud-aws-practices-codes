e1bc7479424033ee902942cb01ad55c6fd67ce6c
Farasat Ali <farasataliazeemi1@gmail.com>
2021-08-07 22:52:46 +0500

    Update step00_simple_lambda_tracing-stack.ts

--- aws-practice-23/Ex00_simple_lambda_tracing/lib/step00_simple_lambda_tracing-stack.ts
+++ aws-practice-23/Ex00_simple_lambda_tracing/lib/step00_simple_lambda_tracing-stack.ts
@@ -1,9 +1,16 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
 
 export class Step00SimpleLambdaTracingStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new lambda.Function(this, "lambda-x-ray-tracing", {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "app.handler",
+      // Enabling X-Ray Tracing
+      tracing: lambda.Tracing.ACTIVE,
+    });
   }
 }
