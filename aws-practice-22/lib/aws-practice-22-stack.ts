154b2e990683b004d994c244812946fae0a02146
Farasat Ali <farasataliazeemi1@gmail.com>
2021-08-07 19:18:17 +0500

    Update aws-practice-22-stack.ts

--- aws-practice-22/lib/aws-practice-22-stack.ts
+++ aws-practice-22/lib/aws-practice-22-stack.ts
@@ -8,8 +8,6 @@ export class AwsPractice22Stack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
     const TimeStreamDB = new timestream.CfnDatabase(this, "TimeStreamDB", {
       databaseName: "TimeStreamDB",
     });
