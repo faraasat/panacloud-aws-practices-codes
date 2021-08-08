cc968ad8214d1eeaa3fb77c80681989c082ed719
Farasat Ali <farasataliazeemi1@gmail.com>
2021-08-08 16:33:47 +0500

    Update ex00_lambda_with_single_layer-stack.ts

--- aws-practice-24/Ex00_lambda_with_single_layer/lib/ex00_lambda_with_single_layer-stack.ts
+++ aws-practice-24/Ex00_lambda_with_single_layer/lib/ex00_lambda_with_single_layer-stack.ts
@@ -1,9 +1,19 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
 
 export class Ex00LambdaWithSingleLayerStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const lambdaLayer = new lambda.LayerVersion(this, "LambdaLayer", {
+      code: lambda.Code.fromAsset("lambda-layer"),
+    });
+
+    new lambda.Function(this, "LambdaWithLambdaLayer", {
+      runtime: lambda.Runtime.NODEJS_12_X, // execution environment
+      code: lambda.Code.fromAsset("lambda-fns"), // code loaded from the "lambda" directory
+      handler: "lambda.handler", // file is "lambda", function is "handler"
+      layers: [lambdaLayer],
+    });
   }
 }
