dffef0e617c6737cbf1cb6ba508beda2077e5459
Farasat Ali <farasataliazeemi1@gmail.com>
2021-08-10 11:49:42 +0500

    Update ex01_lambda_destination_with_different_destinations-stack.ts

--- aws-practice-26/Ex01_lambda_destination_with_different_destinations/lib/ex01_lambda_destination_with_different_destinations-stack.ts
+++ aws-practice-26/Ex01_lambda_destination_with_different_destinations/lib/ex01_lambda_destination_with_different_destinations-stack.ts
@@ -1,7 +1,7 @@
 import * as cdk from "@aws-cdk/core";
 import * as lambda from "@aws-cdk/aws-lambda";
-import * as event from "@aws-cdk/aws-events";
-import * as target from "@aws-cdk/aws-events-targets";
+// import * as event from "@aws-cdk/aws-events";
+// import * as target from "@aws-cdk/aws-events-targets";
 import * as destinations from "@aws-cdk/aws-lambda-destinations";
 import * as apigw from "@aws-cdk/aws-apigateway";
 import * as subs from "@aws-cdk/aws-sns-subscriptions";
