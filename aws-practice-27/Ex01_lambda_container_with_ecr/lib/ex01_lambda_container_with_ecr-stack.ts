e9c6293214d27c79d0ec56ea6e82f88078793682
Farasat Ali <farasataliazeemi1@gmail.com>
2021-08-10 12:06:58 +0500

    Update ex01_lambda_container_with_ecr-stack.ts

--- aws-practice-27/Ex01_lambda_container_with_ecr/lib/ex01_lambda_container_with_ecr-stack.ts
+++ aws-practice-27/Ex01_lambda_container_with_ecr/lib/ex01_lambda_container_with_ecr-stack.ts
@@ -1,9 +1,26 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import { DockerImageAsset } from "@aws-cdk/aws-ecr-assets";
 
 export class Ex01LambdaContainerWithEcrStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //The directory ecr-lambda must include a Dockerfile
+    const asset = new DockerImageAsset(this, "EcrImage", {
+      directory: "ecr-lambda",
+    });
+
+    const ecrLambda = new lambda.DockerImageFunction(
+      this,
+      "LambdaFunctionECR",
+      {
+        code: lambda.DockerImageCode.fromEcr(asset.repository, {
+          tag: "<Tag of the image>",
+        }),
+      }
+    );
+
+    asset.repository.grantPull(ecrLambda);
   }
 }
