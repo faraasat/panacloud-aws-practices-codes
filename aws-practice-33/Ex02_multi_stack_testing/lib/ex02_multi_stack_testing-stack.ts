8a1a704d5091169c6dad94937ddea73b4d9acdb9
Farasat Ali <farasataliazeemi1@gmail.com>
2021-08-12 18:59:03 +0500

    Update ex02_multi_stack_testing-stack.ts

--- aws-practice-33/Ex02_multi_stack_testing/lib/ex02_multi_stack_testing-stack.ts
+++ aws-practice-33/Ex02_multi_stack_testing/lib/ex02_multi_stack_testing-stack.ts
@@ -1,9 +1,24 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as my_construct from "../my_construct/my_construct";
 
 export class Ex02MultiStackTestingStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new lambda.Function(this, "testLambda", {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: new lambda.InlineCode(
+        "exports.handler = async () => {\nconsole.log('hello world');\n};"
+      ),
+      handler: "index.handler",
+      memorySize: 1024,
+    });
+
+    cdk.Tags.of(this).add("hello", "world");
+
+    new my_construct.my_construct(this, "custom_construct", {
+      functionName: "my_function",
+    });
   }
 }
