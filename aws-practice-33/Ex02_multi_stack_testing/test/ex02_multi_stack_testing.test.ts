01ec04301de634b0159a64a04419dc8d8243a67a
Farasat Ali <farasataliazeemi1@gmail.com>
2021-08-12 19:00:41 +0500

    Update ex02_multi_stack_testing.test.ts

--- aws-practice-33/Ex02_multi_stack_testing/test/ex02_multi_stack_testing.test.ts
+++ aws-practice-33/Ex02_multi_stack_testing/test/ex02_multi_stack_testing.test.ts
@@ -1,13 +1,64 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as Ex02MultiStackTesting from '../lib/ex02_multi_stack_testing-stack';
+import * as cdk from "@aws-cdk/core";
+import * as Step02MultiStackTesting from "../lib/ex02_multi_stack_testing-stack";
+import "@aws-cdk/assert/jest";
+import { SynthUtils } from "@aws-cdk/assert";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as my_construct from "../my_construct/my_construct";
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new Ex02MultiStackTesting.Ex02MultiStackTestingStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test("TEST MAIN STACK", () => {
+  const app = new cdk.App();
+  const stack = new Step02MultiStackTesting.Ex02MultiStackTestingStack(
+    app,
+    "MyTestStack"
+  );
+
+  expect(stack).toCountResources("AWS::Lambda::Function", 2);
+
+  expect(stack).toHaveResource("AWS::Lambda::Function", {
+    Handler: "index.handler",
+    MemorySize: 1024,
+  });
+
+  expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+});
+
+test("MOCK A FUNCTION IN MAIN STACK", () => {
+  const app = new cdk.App();
+
+  const mockTagsAspect = jest.spyOn(cdk.Tags, "of");
+
+  const stack = new Step02MultiStackTesting.Ex02MultiStackTestingStack(
+    app,
+    "MyTestStack"
+  );
+
+  expect(mockTagsAspect).toBeCalledTimes(1);
+});
+
+test("TEST IF YOUR CUSTOM CONSTRUCT IS RUNNING IN THE MAIN STACK", () => {
+  const app = new cdk.App();
+  const stack = new Step02MultiStackTesting.Ex02MultiStackTestingStack(
+    app,
+    "MyTestStack"
+  );
+
+  expect(stack).toCountResources("AWS::Lambda::Function", 2);
+
+  expect(stack.node.tryFindChild("custom_construct")).not.toBeUndefined();
+  expect(stack.node.tryFindChild("testLambda")).not.toBeUndefined();
+
+  expect(stack.node.tryFindChild("testLambda1")).toBeUndefined();
+});
+
+test("TESTING CHANGES THAT YOUR CUSTOM_STACK MAKES IN THE MAIN STACK", () => {
+  const app = new cdk.App();
+
+  const stack = new Step02MultiStackTesting.Ex02MultiStackTestingStack(
+    app,
+    "MyTestStack"
+  );
+
+  expect(stack).toCountResources("AWS::DynamoDB::Table", 1);
+
+  expect(stack.node.tryFindChild("table")).not.toBeUndefined();
 });
