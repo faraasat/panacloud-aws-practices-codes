bcd50ba782e646b7d96df03ac7bdc2ea492ca263
Farasat Ali <farasataliazeemi1@gmail.com>
2021-08-13 11:37:42 +0500

    Updated frontend

--- aws-practice-34/frontend/gatsby-browser.js
+++ aws-practice-34/frontend/gatsby-browser.js
@@ -1,7 +1,3 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
+import "./src/styles/global.css"
+import wrapRootElement  from './src/wrappers/wrapRootElement';
+export {wrapRootElement};
