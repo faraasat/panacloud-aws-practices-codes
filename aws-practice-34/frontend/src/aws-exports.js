3c503aac2556c808fcc46b574607c0c2047d87db
Farasat Ali <farasataliazeemi1@gmail.com>
2021-08-14 15:31:37 +0500

    Create aws-exports.js

---
+++ aws-practice-34/frontend/src/aws-exports.js
@@ -0,0 +1,13 @@
+const awsmobile = {
+  aws_project_region: "us-east-1",
+  aws_appsync_graphqlEndpoint: "https://dsdasdasd",
+  aws_appsync_region: "us-east-1",
+  aws_appsync_authenticationType: "AMAZON_COGNITO_USER_POOLS",
+  Auth: {
+    region: "us-east-1",
+    userPoolId: "asasdasdasd",
+    userPoolWebCLientId: "adsdsadasdasdsa",
+  },
+}
+
+export default awsmobile
