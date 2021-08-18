07998387421c77173cc2cbe640cc9dbcc98e7e18
Farasat Ali <farasataliazeemi1@gmail.com>
2021-08-18 10:51:16 +0500

    Created Ex02_Save_Logs_in_S3

---
+++ aws-practice-41/Ex02_Save_Logs_in_S3/test/ex02_save_logs_in_s3.test.ts
@@ -0,0 +1,13 @@
+import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as Ex02SaveLogsInS3 from '../lib/ex02_save_logs_in_s3-stack';
+
+test('Empty Stack', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new Ex02SaveLogsInS3.Ex02SaveLogsInS3Stack(app, 'MyTestStack');
+    // THEN
+    expectCDK(stack).to(matchTemplate({
+      "Resources": {}
+    }, MatchStyle.EXACT))
+});
