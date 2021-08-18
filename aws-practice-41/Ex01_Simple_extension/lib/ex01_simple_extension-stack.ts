311db8d0e8ac403c12f3bde38cb481fc7281166f
Farasat Ali <farasataliazeemi1@gmail.com>
2021-08-18 10:52:21 +0500

    Update ex01_simple_extension-stack.ts

--- aws-practice-41/Ex01_Simple_extension/lib/ex01_simple_extension-stack.ts
+++ aws-practice-41/Ex01_Simple_extension/lib/ex01_simple_extension-stack.ts
@@ -1,9 +1,28 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as path from "path";
 
 export class Ex01SimpleExtensionStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    /========================================
+    // Lambda Layer that will be used as Extension
+    //========================================
+    const lambdaLayer = new lambda.LayerVersion(this, "ext-layer", {
+      code: lambda.Code.fromAsset("lambda-layers"),
+    });
+
+    //========================================
+    // Lambda Func
+    //========================================
+
+    const sendfunc = new lambda.Function(this, "sendlog", {
+      code: lambda.Code.fromAsset("lambda"),
+      runtime: lambda.Runtime.NODEJS_12_X,
+      handler: "hello.handler",
+      memorySize: 1024,
+      layers: [lambdaLayer],
+    });
   }
 }
