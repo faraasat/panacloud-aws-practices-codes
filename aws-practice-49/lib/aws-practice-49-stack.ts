10a0b8ecdc662c0928cb16fca9fb81f62935cb23
Farasat Ali <farasataliazeemi1@gmail.com>
2021-08-19 12:57:24 +0500

    Update aws-practice-49-stack.ts

--- aws-practice-49/lib/aws-practice-49-stack.ts
+++ aws-practice-49/lib/aws-practice-49-stack.ts
@@ -1,9 +1,26 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as ivs from "@aws-cdk/aws-ivs";
+const cdk_spa_deploy_1 = require("cdk-spa-deploy");
+
+const fs = require("fs");
 
 export class AwsPractice49Stack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const liveCh = new ivs.CfnChannel(this, "Channel", {
+      name: "liveChanel",
+      type: "BASIC", //Avaialbe options are STANDARD & BASIC
+      latencyMode: "LOW", //Avaialbe options are    NORMAL | LOW
+      //   authorized : true   *   Authorized playback requests with Tokens (JWTs) (https://docs.aws.amazon.com/ivs/latest/userguide/private-channels.html)
+    });
+
+    // const keyPair = new ivs.PlaybackKeyPair(this, 'PlaybackKeyPair', {
+    //   publicKeyMaterial: "myPublicKeyPemString",
+    // });
+
+    new cdk.CfnOutput(this, "playback URL", {
+      value: liveCh.attrPlaybackUrl,
+    });
   }
 }
